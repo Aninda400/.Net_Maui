30a147cc14aff021dc0f42a8ff1882e484833a30
Aninda Dhar <a1808432@student.adelaide.edu.au>
2022-07-10 00:29:51 +0930

    New project : Bill payment
    Creating an app that will calculate the bill of each people in a restaurant and also add tips where necessary. I am taking help from multiple sources as I am very new to the technology. Cheers

---
+++ Bill payment/Bill Payment/Bill Payment/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Bill_Payment;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Bill payment/Bill Payment/Bill Payment/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Bill_Payment;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
